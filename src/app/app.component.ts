e62094cface354cebfeff81b6c244f653a8fa5b0
SENURA INDEEWARA <senuraindeewara@gmail.com>
2025-04-13 22:16:39 +0530

    uodate landig page (error)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomerLandingComponent } from "./page/customer-landing/customer-landing.component";
+import { LandingPageComponent } from "./common/landing-page/landing-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomerLandingComponent],
+  imports: [RouterOutlet, CustomerLandingComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
