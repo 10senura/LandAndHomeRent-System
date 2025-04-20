18fbd60dcc6e1ca3961df76722e8bfa11b256015
SENURA INDEEWARA <senuraindeewara@gmail.com>
2025-04-20 21:45:11 +0530

    update login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CustomerLandingComponent } from "./page/customer-landing/customer-landing.component";
-import { LandingPageComponent } from "./common/landing-page/landing-page.component";
 import { LoginPageComponent } from "./common/login-page/login-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CustomerLandingComponent, LandingPageComponent, LoginPageComponent],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
